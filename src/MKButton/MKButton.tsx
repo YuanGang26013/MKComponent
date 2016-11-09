6a90fdd26ca7d8c7e8ea40a2a909e5b9c2b1afcf
YuanGang <262013@qq.com>
2016-11-09 16:02:41 +0800

    新增按钮组件

---
+++ src/MKButton/MKButton.tsx
@@ -0,0 +1,10 @@
+/**
+ * Created by Gene on 2016/11/9.
+ */
+
+export default function(props) {
+
+    return (
+        <button class="mk-button">{props.text}</button>
+    )
+}
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+/**
+ * Created by Gene on 2016/11/9.
+ */
+
+import MKButton from './MKButton/MKButton';
+
+export {
+    MKButton
+};
