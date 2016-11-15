e96345d58f620048a2fd9e36a9582d70527c3c0c
YuanGang <262013@qq.com>
2016-11-15 11:01:33 +0800

    按钮组件新增onClick

--- src/MKButton/MKButton.tsx
+++ src/MKButton/MKButton.tsx
@@ -2,8 +2,9 @@
  * Created by Gene on 2016/11/9.
  */
 
-export default function(props) {
+export default function(props:MKComponentProps.MKButtonProps) {
+
     return (
-        <button className="mk-button">{props.text}</button>
+        <button className="mk-button" onClick={props.onClick}>{props.text}</button>
     )
 }
